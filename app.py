214abc447d1f9da83ede4803e79344ea03e6f7af
James Dillard <jamesedwarddillard@gmail.com>
2015-11-15 14:36:13 -0800

    tested app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello Jimmy!"
 
+@app.route('/<year>/<month>')
+def hello_selected_month(year, month):
+	return "How many jobs were added in {}, {}?".format(month, year)
+
 if __name__ == '__main__':
     app.run()
