7f02232a09bfca91b66bdf1c3bbf6cc1a7be2bd4
James Dillard <jamesedwarddillard@gmail.com>
2015-11-22 15:55:04 -0800

    added requests

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -11,7 +12,18 @@ from models import Result
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	errors = []
+	results = {}
+	if request.method == 'POST':
+		try:
+			url = request.form['url']
+			r = requests.get(url)
+			print r.text
+		except:
+			errors.append(
+				"Unable to get URL. Please make sure it's valid and try again"
+				)
+	return render_template('index.html', errors=errors, results=results)
 
 @app.route('/<year>/<month>')
 def hello_selected_month(year, month):
