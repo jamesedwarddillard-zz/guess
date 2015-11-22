9db9d21a4ad121a9320e9af7d257cde569f2e7f2
James Dillard <jamesedwarddillard@gmail.com>
2015-11-22 15:37:56 -0800

    added a basic template

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy.dialects.postgresql import JSON
 
 class Result(db.Model):
 	__tablename__ = 'results'
+	
 	id = db.Column(db.Integer, primary_key = True)
 	url = db.Column(db.String())
 	result_all = db.Column(JSON)
