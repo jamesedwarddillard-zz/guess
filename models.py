8d1f14eb7befc6d1b901c3571d3e7a760efc1e42
James Dillard <jamesedwarddillard@gmail.com>
2015-11-22 16:24:47 -0800

    added twitter api work

--- models.py
+++ models.py
@@ -2,17 +2,17 @@ from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
 class Result(db.Model):
-	__tablename__ = 'results'
-	
+	__tablename__ = 'jobs'
+
 	id = db.Column(db.Integer, primary_key = True)
-	url = db.Column(db.String())
-	result_all = db.Column(JSON)
-	result_no_stop_words = db.Column(JSON)
+	month = db.Column(db.Integer)
+	year = db.Column(db.Integer)
+	jobs = db.Column(db.Integer)
 
 	def __init__(self, url, result_all, result_no_stop_words):
-		self.url = url
-		self.result_all = result_all
-		self.result_no_stop_words = result_no_stop_words
+		self.month = month
+		self.year = year
+		self.jobs = jobs
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
