d361476e429fb27aec69ab09ea673f1547acfd93
James Dillard <jamesedwarddillard@gmail.com>
2015-12-07 21:31:09 -0800

    moved in bls_reader stuff

---
+++ bls_reader/bls_api.py
@@ -0,0 +1,13 @@
+bls_api_url = 'http://api.bls.gov/publicAPI/v2/timeseries/data/'
+bls_api_headers = {'Content-type': 'application/json'}
+
+#how to create series ids http://www.bls.gov/developers/api_faqs.htm#signatures3
+
+prefix = 'CE'
+seasonal = 'S'
+supersector = '00'
+industry = '000000'
+data_type = '01'
+
+bls_api_series_id = [prefix+seasonal+supersector+industry+data_type]
+
